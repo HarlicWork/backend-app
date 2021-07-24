671f43870467a8a04eab8a8caac8e024785225b6
HarlicWork <harlic.work@gmail.com>
2021-07-25 01:40:21 +0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'abace4085bff3dc20dc42784d0d668e9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d7fb902-3d23-4ed7-a190-13cea9dc7321'
+};
